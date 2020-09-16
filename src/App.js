b2ef8c4a21c1de76825390becaed56997a7b685e
ofkings0 <xusihu18@gmail.com>
2020-09-16 16:53:51 +0100

    menu-items naviagte withRouter to HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>hats lol</h1>
+  </div>
+);
+
 function App() {
   return (
-    <Homepage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch> 
+
+    </div>
   );
 }
 
