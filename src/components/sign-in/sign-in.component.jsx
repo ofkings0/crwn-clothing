c32f804bfae37d3efd3a0e804e496228004fc90a
ofkings0 <xusihu18@gmail.com>
2020-09-20 20:44:53 +0100

    finished signin handleSubmit to log in with existing email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -15,10 +15,19 @@ class SignIn extends React.Component{
 		};
   };
 
-  handleSubmit = event => {
-      event.preventDefault();
+  handleSubmit = async event => {
+	  event.preventDefault();
+	  
+	  const { email, password } = this.state;
 
-      this.setState({email:'', password:''})
+		try {
+			await auth.signInWithEmailAndPassword(email, password)
+		
+			this.setState = {
+				email: '',
+				password: ''
+			};
+		}
   }
 
   handleChange = event => {
