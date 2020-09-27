12ef7fc0993bf394eed298aceefc47b1e4a4c886
ofkings0 <xusihu18@gmail.com>
2020-09-27 14:11:50 +0100

    added addItem and removeItem to checkoutItem component

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -2,7 +2,9 @@
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN', 
     ADD_ITEM: 'ADD_ITEM',
-    TOTAL_ITEMS: 'TOTAL_ITEMS'
+    TOTAL_ITEMS: 'TOTAL_ITEMS',
+    CLEAR_ITEM: 'CLEAR_ITEM',
+    REMOVE_ITEM: 'REMOVE_ITEM'
 };
   
 export default CartActionTypes;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,7 +9,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
     )
-  } //else {
+  } else {
     return [...cartItems, { ...cartItemToAdd, quantity: 1}]
-  //}
+  }
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find( //returns the first cartItem that matches the condition
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== existingCartItem.id)
+  }
+
+  return cartItems.map(
+    cartItem => 
+    cartItem.id === cartItemToRemove.id ? 
+    { ...cartItem, quantity: cartItem.quantity -1 }
+    : cartItem
+  )
 }
